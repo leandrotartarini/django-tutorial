29a73dc4632984eb38a46892dfe7cd7c392e5ee4
leandrotartarini <ldo.tartarini@gmail.com>
2021-11-18 08:49:21 +0000

    Fixed url files

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+from snippets import views
 
 urlpatterns = [
-    path('', include('snippets.urls')),
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail)
 ]
