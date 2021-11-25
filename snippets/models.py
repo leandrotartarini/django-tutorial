72d43cf9bc7e2177c4ef6cae8eec56a3ba7f5697
leandrotartarini <ldo.tartarini@gmail.com>
2021-11-25 09:12:00 +0000

    Refactor

--- snippets/models.py
+++ snippets/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models.base import Model
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 from pygments.lexers import get_lexer_by_name
@@ -10,9 +9,6 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([item[1][0], item[0]] for item in LEXERS)
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
-owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
-highlighted = models.TextField()
-
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
@@ -20,6 +16,8 @@ class Snippet(models.Model):
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ['created']
--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,15 +3,6 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
-
-    class Meta:
-        model = User
-        fields = ['url', 'id', 'username', 'snippets']
-
-
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
 
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -20,20 +11,9 @@ class SnippetSerializer(serializers.HyperlinkedModelSerializer):
         model = Snippet
         fields = ['url', 'id', 'highlight', 'owner' 'title', 'code', 'linenos', 'language', 'style']
 
-    def create(self, validated_data):
-        """
-        Create and return a new 'Snippet' instance, given the validated data.
-        """
-        return Snippet.objects.create(**validated_data)
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing 'Snippet' instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
